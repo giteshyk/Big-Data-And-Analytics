6b517e01204bfab2f26479222661f57609dadfe7
Gitesh Katre <61425961+giteshyk@users.noreply.github.com>
2021-03-16 21:29:13 +0530

    Create download_zip_files_with_specific_files.py
    So, I was assigned a task to work with log files from certain test cases, the thing was I was made to pull the log files from a shared location and those were zipped after each run.
    
    With this code I can now extract such files , although it does take time to execute.

---
+++ download_zip_files_with_specific_files.py
@@ -0,0 +1,36 @@
+import re
+import glob
+import pandas as pd
+import datetime
+import time
+import os
+import shutil
+import sys
+import socket
+from zipfile import ZipFile
+from zipfile import BadZipFile
+from shutil import copyfile
+
+#get all the zip files from certain location with glob,in my case a shared location 
+raw_data_files = glob.glob(r'\\vmspfsfsbg06.gar.corp.intel.com\HexaLogs\XPIV\BDC\*\*\*\*.zip') 
+
+#list that will have locaitons of the required files
+zip_with_summary_files = []
+
+for item in raw_data_files:
+    #try will ignore the files that are corrupt,otherwise code exits with BadZipFile error
+    try:
+        with ZipFile(raw_data_files[item], 'r') as zip:
+            file_list = zip.namelist()
+            
+        for item_1 in file_list:
+            #I needed files which had 'summary' in their names
+            if(item_1.find("summary") != -1):
+                zip_with_summary_files.append(item)
+            
+    except BadZipFile:
+        pass
+
+   
+for item in zip_with_summary_files:
+    shutil.copy(item, r'\local\path\here')
