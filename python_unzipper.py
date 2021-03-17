73554119a1b0b9fbe68c7db3439fef301038561e
Gitesh Katre <61425961+giteshyk@users.noreply.github.com>
2021-03-17 22:04:54 +0530

    Create python_unzipper.py

---
+++ python_unzipper.py
@@ -0,0 +1,10 @@
+import glob
+import zipfile
+from zipfile import ZipFile
+
+#get the paths of the zip files
+raw_data_files = glob.glob('path\to\zip_files\*.zip')
+
+for item in raw_data_files:
+    with zipfile.ZipFile(item,"r") as my_zip_file:
+        my_zip_file.extractall("target\path\to\log_files_" + item[34:60])
